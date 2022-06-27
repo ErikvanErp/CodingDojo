141be1c3a13ba4a6a6c6dab659aacf8b492f31b9
erikvanerp <jhamvanerp@gmail.com>
2022-06-27 10:42:52 -0400

    Video Preview assignment completed

---
+++ WebFund/Week2/Mon/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function videoPlay(element){
+    console.log("mouse over")
+    element.play();
+}
+
+function videoPause(element){
+    console.log("mouse leave");
+    element.pause();
+}
