98b153ce46982a50be14225ba7fcd56f75ee00fb
erikvanerp <jhamvanerp@gmail.com>
2022-07-14 00:53:43 -0400

    user crud assignment completed

--- python/flask_mysql/db_connections/users_crud/server.py
+++ python/flask_mysql/db_connections/users_crud/server.py
@@ -5,18 +5,35 @@ from users import User
 app = Flask(__name__)
 
 @app.route("/")
-def read():
-    return render_template("read.html", users=User.read_all())
-    
-@app.route("/add_user")
+def readall():
+    return render_template("readall.html", users=User.read_all())
+
+@app.route("/user/add")
 def add_user():
     return render_template("create.html")
 
-@app.route("/create_user", methods=['POST'])
-def create_user():
-    User.insert(request.form)
-    return redirect('/')
+@app.route("/user/insert", methods=['POST'])
+def insert_user():
+    new_id = User.insert(request.form)
+    return redirect(f"/user/show/{new_id}")
 
+@app.route('/user/show/<int:user_id>')
+def show_user(user_id):
+    return render_template("readone.html", user=User.read(user_id))
+
+@app.route('/user/edit/<int:user_id>')
+def edit_user(user_id):
+    return render_template("edit.html", user=User.read(user_id))
+
+@app.route('/user/update', methods=['POST'])
+def update_user():
+    User.update(request.form)
+    return redirect(f"/user/show/{request.form['user_id']}")
+
+@app.route("/user/delete/<int:user_id>")
+def delete_user(user_id):
+    User.delete(user_id)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- python/flask_mysql/db_connections/users_crud/users.py
+++ python/flask_mysql/db_connections/users_crud/users.py
@@ -21,9 +21,27 @@ class User():
 
     @classmethod
     def insert(cls, form):
-        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES ( %(fname)s, %(lname)s, %(email)s, NOW(), NOW() )"
+        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES ( %(fname)s, %(lname)s, %(email)s, NOW(), NOW() );"
                 
         return connectToMySQL('users').query_db(query, form)
+
+    @classmethod
+    def read(cls, user_id):
+        query = "SELECT * FROM users WHERE id = %(user_id)s;"
+
+        return connectToMySQL('users').query_db(query, {"user_id" : user_id})[0]
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name=%(fname)s, last_name=%(lname)s, email=%(email)s, updated_at=NOW() WHERE id=%(user_id)s;"
+
+        return connectToMySQL('users').query_db(query, data)
+
+    @classmethod
+    def delete(cls, user_id):
+        query = "DELETE FROM users WHERE id = %(user_id)s"
+        
+        return connectToMySQL('users').query_db(query, {"user_id" : user_id})
 
     @property
     def full_name(self):
