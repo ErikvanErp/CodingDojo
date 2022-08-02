c9f49294ad1ece881da9d4a0e12b10720dfb9c7f
erikvanerp <jhamvanerp@gmail.com>
2022-08-02 12:26:26 -0400

    algo linkes list

--- Java/fundamentals/MyInfo.java
+++
@@ -1,6 +0,0 @@
-public class MyInfo {
-    public static void main(String[] args){
-        System.out.println("My name is Erik");
-        System.out.println("My hometown is Uden, Netherlands");
-    }
-}
