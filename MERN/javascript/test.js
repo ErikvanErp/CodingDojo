1b7459d0586e7300defed9fe7ccd315c017f93c4
Erik van Erp <jhamvanerp@gmail.com>
2022-08-28 18:20:58 -0400

    convert to arrow ES6 assignment

--- MERN/javascript/test.js
+++ MERN/javascript/test.js
@@ -1,17 +1,22 @@
-const person = { 
-        firstName: 'Bob', 
-        lastName: 'Marley', 
-        email: 'bob@marley.com', 
-        password: 'sekureP@ssw0rd9', 
-        username: 'barley', 
-        createdAt: 1543945177623
-    };
-    const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+class Deck {
+    constructor() {
+      const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+      const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+      const deck = [];
+      for (const suit of suits) {
+        for (const face of faces) {
+          deck.push(this.createCard(suit, face));
+        }
+      }
+      this.deck = deck;
+    }
 
-    // AFTER ES6
-const { email, password } = person;
-const [firstAnimal,,thirdAnimal] = animals;
-console.log(email, password);
-// => bob@marley.com
-console.log(firstAnimal, thirdAnimal);
-// => horse
+    createCard(suit, face){
+        return {"suit": suit, "face": face};
+    }
+  }
+
+  const deck = new Deck();
+  console.log(deck);
+  
+  
