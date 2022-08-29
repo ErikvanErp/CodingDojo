37560a2f91930ad16125723df3ca99054668b010
Erik van Erp <jhamvanerp@gmail.com>
2022-08-29 16:32:50 -0400

    Ninja class js

---
+++ MERN/javascript/Ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);        
+    }
+    showStats(){
+        console.log(`name: ${ this.name }, strength: ${ this.strength }, speed: ${ this.speed }, health: ${ this.health }.`);
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 50);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
+
