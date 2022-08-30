176f0f0411c847807f5f66df4a6ba0fffc0fdf85
Erik van Erp <jhamvanerp@gmail.com>
2022-08-30 11:22:42 -0400

    algos, insertionsort

---
+++ Algo/w11d2_insertion_sort.js
@@ -0,0 +1,19 @@
+function insertionSort(array){
+    for(let i = 0; i < array.length - 1; i++){
+        let j = i + 1;
+        while(j > 0 && array[j] < array[j -1]){
+            [array[j - 1], array[j]] = [array[j], array[j - 1]];
+            j--;
+        }
+    }
+    return array;
+}
+
+
+console.log(insertionSort([5, 6, 2, 9, 10, 3, 0, 2]));
+console.log(insertionSort([5]));
+console.log(insertionSort([]));
+console.log(insertionSort([5, 5, 5, 5]));
+console.log(insertionSort([1, 2, 3, 4]));
+console.log(insertionSort([4, 3, 2, 1]));
+
--- MERN/javascript/test.js
+++ MERN/javascript/test.js
@@ -1,7 +1,15 @@
-nums = [1, 2, 3, 4, 5, 6, 7]
-console.log(nums.sort((a,b) => a-b));  
-console.log(nums.map( (x, i) => x**(i+1) ));  
-console.log(nums.map( (x,i,arr) => x + arr[i+1])
-                .filter((x,i,arr) => !(i == arr.length - 1)))
-console.log(nums.reduce((x,y) => x * y, 2))
-  
+// here we have a function called "Outer"
+const counter = (() => {
+  let count = 0; 
+  return () => {
+    count++;
+    return count;
+  }
+})();
+
+console.log(counter());              
+console.log(counter());              
+console.log(counter());              
+console.log(counter());              
+    
+
