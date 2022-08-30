cb4190966b01dc63dada317f42ef374e284f6641
Erik van Erp <jhamvanerp@gmail.com>
2022-08-30 16:37:12 -0400

    PropItUp assignment (props)

--- MERN/React/first-react-project/src/App.js
+++ MERN/React/first-react-project/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
 import React from 'react';
-import MyComponent from './components/myComponent';
+import PersonCard from './components/PersonCard';
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
-            <MyComponent fName = "Erik" lName = "van Erp"/>
-            <MyComponent fName = "River" lName = "van Erp"/>
-            <MyComponent fName = "Susan" lName = "Qu"/>
+            <PersonCard firstName="Jane" lastName="Doe" age={ 45 } hairColor="Black"/>
+            <PersonCard firstName="John" lastName="Smith" age={ 88 } hairColor="Brown"/>
+            <PersonCard firstName="Millard" lastName="Fillmore" age={ 50 } hairColor="Brown"/>
+            <PersonCard firstName="Maria" lastName="Smith" age={ 62 } hairColor="Brown"/>
         </div>
     );
 }
