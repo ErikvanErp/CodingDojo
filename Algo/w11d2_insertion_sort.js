81d79e5a6c2d2d3052c73052d18bca4c5045113e
Erik van Erp <jhamvanerp@gmail.com>
2022-09-01 12:12:48 -0400

    Algo: mergesort

--- Algo/w11d2_insertion_sort.js
+++ Algo/w11d2_insertion_sort.js
@@ -1,10 +1,10 @@
 const insertionSort = (array) => {
-    for(let i = 0; i < array.length - 1; i++){
-        let j = i + 1;
-        while(j > 0 && array[j] < array[j -1]){
-            [array[j - 1], array[j]] = [array[j], array[j - 1]];
+    for(let i = 1; i < array.length; i++){
+        let j = i - 1;
+        while(j > 0 && array[i] < array[j]){
             j--;
         }
+        [array[i], array[j + 1]] = [array[j + 1], array[i]];
     }
     return array;
 }
